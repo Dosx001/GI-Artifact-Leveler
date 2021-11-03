3d22f43fa570e79dfd23a09ba2172b970407b694
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2021-11-03 12:05:01 -0700

    Switched PIL to mss package

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
-from PIL import ImageGrab, Image
+import mss
+import mss.tools
+import numpy
 import pytesseract
 
 def main():
-    file = "fullscreen.png"
-    artSet = (1770, 210, 2220, 255)
-    mainStat = (1750, 270, 2000, 440)
-    subStats = (1750, 550, 2150, 725)
-    im = ImageGrab.grab(bbox = artSet)
-    im.save(file)
+    artSet = {'top': 210, 'left': 1770, 'width': 450, 'height': 45}
+    mainStat = {'top': 270, 'left': 1750, 'width': 250, 'height': 170}
+    subStats = {'top': 550, 'left': 1750, 'width': 400, 'height': 175}
+
+    sct = mss.mss()
+    img = sct.grab(mainStat)
+    mss.tools.to_png(img.rgb, img.size, output='img.png')
+
     pytesseract.pytesseract.tesseract_cmd = r'D:\Users\Dosx001\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
-    text = pytesseract.image_to_string(Image.open(file))
+    text = pytesseract.image_to_string(numpy.asarray(img))
     print(text)
     print(text.split('\n'))
 
