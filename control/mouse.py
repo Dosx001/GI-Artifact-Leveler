05d3d5e101b97f88a9ac8d58a75b66d06b617be9
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2021-11-20 07:49:14 -0800

    Added ArgumentParser

--- control/mouse.py
+++ control/mouse.py
@@ -1,19 +1,21 @@
+from time import sleep
 import pyautogui as ag
 
 class Mouse:
-    def __init__(self, post = 0, items = 0):
-        self.post = post
-        self.items = items
+    def __init__(self, col, total):
+        self.col = col
+        self.total = total
 
     def move(self):
-        if self.post == 10:
-            self.post = 0
+        if self.col == 10:
+            self.col = 0
             ag.moveRel(-1420, 6)
             for i in range(9):
                 ag.scroll(-1)
         else:
-            self.post += 1
+            self.col += 1
             ag.moveRel(142, 0)
 
     def click(self):
         ag.click(clicks=2)
+        sleep(.1)
--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from processText.img2text import Img2Text
 from control.mouse import Mouse
+from argparse import ArgumentParser
 import asyncio
 
-def main():
+def main(args):
+    mu = Mouse(args.column, args.total)
     itt = Img2Text()
-    mu = Mouse(1)
-    for i in range(31):
+    for i in range(1000):
         mu.click()
         print(asyncio.run(itt.getText()))
         mu.click()
         mu.move()
 
 if __name__ == "__main__":
-    main()
+    parser = ArgumentParser()
+    parser.add_argument('--column', type=int, default=0, help='An optional integer argument')
+    parser.add_argument('--total', type=int, default=0, help='An optional integer argument')
+    main(parser.parse_args())
